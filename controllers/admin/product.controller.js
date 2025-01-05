4f01271c260184d6ed8c3c2a2020c56b9ae12a89
PhatNgo03 <phatngo1908@gmail.com>
2025-01-05 11:32:52 +0700

    filterStatus item product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,13 +1,49 @@
 // [GET] /admin/products
 const Product = require("../../models/product.model")
 module.exports.index =  async(req, res) => {
-  const products = await Product.find({
-    // status:"active",
+
+
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: ""
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: ""
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: ""
+    }
+  ]
+  // console.log(req.query.status);
+  let find = {
     delete : false
-  })
-  console.log(products);
+  }
+
+  //filterStatus
+  if(req.query.status){
+    const index = filterStatus.findIndex(item => item.status == req.query.status); //Tim index status == status click
+    filterStatus[index].class = "active";
+  }else{
+    const index = filterStatus.findIndex(item => item.status == ""); //Tim index status == status click
+    filterStatus[index].class = "active";
+  }
+
+  //statusProduct
+  if(req.query.status){
+    find.status = req.query.status;
+  }
+
+  const products = await Product.find(find)
+  // console.log(products);
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Trang danh sách sản phẩm",
-    products : products
+    products : products,
+    filterStatus: filterStatus
 })
 }

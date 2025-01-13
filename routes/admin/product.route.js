3d6cb599ba22d689ed30a6d26d206b44a7821084
PhatNgo03 <phatngo1908@gmail.com>
2025-01-13 14:41:42 +0700

    upload image preview before create item product

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/product.controller")
+const multer = require("multer");
+const storageMulter = require("../../helpers/storageMulter");
+const upload = multer({ storage : storageMulter()}); // Đường dẫn để lưu file upload
 
 router.get("/", controller.index);
 router.patch("/change-status/:status/:id", controller.changeStatus); //truyen status dong va id dong
 router.patch("/change-multi", controller.changeMulti);
 router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create); // return giao dien
-router.post("/create", controller.createPost); // return create item
+router.post("/create", upload.single("thumbnail"), controller.createPost); // return create item
+
 module.exports = router;

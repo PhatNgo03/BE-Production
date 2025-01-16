46d9d71e63c29c2ac437ad2e9719a0205a1cc0ff
PhatNgo03 <phatngo1908@gmail.com>
2025-01-16 21:36:55 +0700

    set up page product-category

---
+++ routes/admin/product-category.route.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../../controllers/admin/product-category.controller");
+const multer = require("multer");
+const upload = multer();
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+const validate = require("../../validates/admin/product-category.validate");
+
+router.get("/", controller.index);
+router.get("/create", controller.create);
+router.post(
+  "/create",
+  upload.single("thumbnail"),
+  uploadCloud.upload,
+  validate.createPost,
+  controller.createPost
+); // return create item
+module.exports = router;

9b5ea75d8791f475f7c2b0796756dfcb8a5b14c6
PhatNgo03 <phatngo1908@gmail.com>
2025-02-05 19:06:24 +0700

    create permissions admin

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -32,3 +32,64 @@ module.exports.createPost = async (req, res) => {
   //     pageTitle: "Tạo mới nhóm quyền",
   // })
 }
+
+
+// [GET] /admin/roles/edit/:"id"
+module.exports.edit =  async(req, res) => {
+  // console.log(req.params.id);
+  try{
+  const find = {
+    delete : false,
+    _id: req.params.id
+  }
+  const roles = await Role.findOne(find);
+
+  res.render("admin/pages/roles/edit", {
+    pageTitle: "Chỉnh sửa nhóm quyền",
+    roles: roles,
+  });
+  } catch(error){
+    req.flash("error", "Đã xảy ra lỗi khi tìm kiếm danh mục sản phẩm!");
+    res.redirect(`${systemConfig.prefixAdmin}/roles`);
+  }
+};
+
+// [PATCH] /admin/products/edit/:"id"
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Role.updateOne({ _id: id }, req.body);
+    req.flash("success", "Cập nhật danh mục sản phẩm thành công!"); 
+  } catch (error) {
+    req.flash("error", "Cập nhật danh mục sản phẩm thất bại!");
+  }
+
+  res.redirect("back");
+};
+
+// [Get] /admin/roles/permission"
+module.exports.permissions = async (req, res) => {
+  let find = {
+    delete :false 
+  }
+  const records = await Role.find(find);
+  res.render("admin/pages/roles/permissions", {
+    pageTitle: "Phân quyền",
+    records: records,
+  });
+};
+
+// [PATCH] /admin/roles/permissionsPatch"
+module.exports.permissionsPatch = async (req, res) => {
+  const permissions = JSON.parse(req.body.permissions);
+  try{
+    for(const item of permissions){
+      await Role.updateOne({_id: item.id}, {permissions: item.permissions})
+    }
+    req.flash("success", "Cập nhật phân quyền thành công!"); 
+    res.redirect("back");
+  }
+  catch(error){
+    req.flash("error", "Cập nhật phân quyền thất bại!");
+  }
+};
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/role.controller")
+const validate = require("../../validates/admin/product-category.validate");
 
 router.get("/", controller.index);
 router.get("/create", controller.create);
 router.post("/create", controller.createPost);
-
+router.get("/edit/:id", controller.edit);
+router.patch(
+  "/edit/:id",
+  controller.editPatch,
+  validate.createPost,
+  );
+router.get("/permissions",controller.permissions);
+router.patch(
+  "/permissions",
+  controller.permissionsPatch,
+  );
 module.exports = router;

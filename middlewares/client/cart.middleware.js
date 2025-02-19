29f4e9ab52df99ce98ebc4b0f3a09306e7508c9b
PhatNgo03 <phatngo1908@gmail.com>
2025-02-19 16:12:45 +0700

    add products in cart

---
+++ middlewares/client/cart.middleware.js
@@ -0,0 +1,21 @@
+const ProductCategory = require("../../models/product-category.model");
+const createTreeHelper = require('../../helpers/createTree');
+const Cart = require("../../models/cart.model");
+
+module.exports.cartId = async(req,res,next) => {
+  // console.log(req.cookies.cartId);
+  if(!req.cookies.cartId){
+    //create cart
+    const cart = new Cart();
+    await cart.save();
+    const expiresCookie = 365 * 24 * 60 * 60 * 1000;
+    res.cookie("cartId", cart.id, {
+      expires: new Date(Date.now() + expiresCookie)
+    });
+  }
+  else {
+    //get cart
+
+  }
+  next(); 
+}
